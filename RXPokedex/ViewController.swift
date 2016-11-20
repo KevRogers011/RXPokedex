2bdb07b7466a0533c632ff3bd4a5e6e2ae3dd5f2
Me <kevinrogers@kevins-macbook.local>
2016-11-20 12:26:03 -0500

    Initial Commit

---
+++ RXPokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RXPokedex
+//
+//  Created by Kevin Rogers on 11/20/16.
+//  Copyright © 2016 RX media Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
